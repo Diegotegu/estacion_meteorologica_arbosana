3997711052260ca7ecc137372275820bfd19aca9
Diego.tejada <diego.tejada@diesia.uhu.es>
2019-09-10 17:03:47 +0200

    toma la cadena que recibe de la estacion, la corta y guarda cada valor en su variable. revisar el corte de las variables, conversion y habilitar el envío. copiar la rutina de reinicio del arduino de WSN deshabilitar el debugger y programar definitivamente.

--- estacion_meteorologica_arbosana/estacion_meteorologica_arbosana.ino
+++ estacion_meteorologica_arbosana/estacion_meteorologica_arbosana.ino
@@ -34,8 +34,8 @@ String respuesta;
 float direccion;
 float velocidad, temperatura, presion, humedad, lluvia, lluvia_intensidad, granizo, granizo_intensidad;
 char buff;
-int comas[20];
-String secciones[20];
+int comas[11];
+String secciones[10];
 bool debugger = true;
 int oneshot = true;
 #define freq_msg 15 //Frequency (in minutes for message)
@@ -47,7 +47,7 @@ void dummy() {
 
 
 void setup() {
-    LowPower.attachInterruptWakeup(RTC_ALARM_WAKEUP, dummy, CHANGE); //For LowPower Bug
+  LowPower.attachInterruptWakeup(RTC_ALARM_WAKEUP, dummy, CHANGE); //For LowPower Bug
   Serial1.begin(19200);
   if (debugger) {
     Serial.begin(19200);
@@ -69,7 +69,7 @@ void setup() {
   if (oneshot == true) {
     // Enable debug prints and LED indication if we are testing
     SigFox.debug();
- }
+  }
 
 
 }
@@ -77,73 +77,55 @@ void setup() {
 void loop() {
   respuesta = "";
   buff = 0;
-  unsigned long t_start;
-  bool end_string =false;
-  bool end_string_time=false;
   Serial.println("enviando petición");
   Serial1.println("0R0");
-  delay(25); 
-  t_start=millis();
+  delay(25);
+
   while (!Serial1.available());
-while (Serial1.available()) {
+  while (Serial1.available()) {
     Serial.print(buff);
-    buff = Serial1.read(); 
-    if (buff==13 && Serial1.read()==10){
-             end_string=true;
-             Serial.println("final de la cadena");
-    }
+    buff = Serial1.read();
     respuesta += buff;
     delay(100);
-    if (millis()-t_start >30000) {
-      Serial.println("limite alcanzado");
-      end_string_time=true;
-      }    
   }
-  //end_string=false;
   Serial.println();
 
-  //delay(000);
-  /*
-
-  for (int i = 0; i <= 20; i++) {
-    comas[i] = 0;
-  }
   int a = 0;
-  for (int i = 0; i < 200; i++) {
+  for (int i = 0; i < respuesta.length(); i++) {
     if (respuesta.charAt(i) == ',') {
       comas[a] = i;
       a++;
     }
   }
-  for (int i = 1; i <= 20; i++) {
+  for (int i = 1; i <= a; i++) {
     secciones[i - 1] = respuesta.substring(comas[i - 1] + 4, comas[i] - 1);
   }
+  
   Serial.println("respuesta recibida, guardando en variables");
-  direccion = secciones[1].toFloat();
-  velocidad = secciones[4].toFloat();
-  temperatura = secciones[6].toFloat();
-  humedad = secciones[8].toFloat();
-  presion = secciones[9].toFloat();
-  lluvia = secciones[10].toFloat(); //lluvia acumulada e intensidad de lluvia
-  lluvia_intensidad = secciones[12].toFloat();
-  granizo = secciones[13].toFloat();//granizo acumulado e intensidad de granizo
-  granizo_intensidad = secciones[15].toFloat();
+  direccion = secciones[0].toFloat();
+  velocidad = secciones[1].toFloat();
+  temperatura = secciones[2].toFloat();
+  humedad = secciones[3].toFloat();
+  presion = secciones[4].toFloat();
+  lluvia = secciones[5].toFloat(); //lluvia acumulada e intensidad de lluvia
+  lluvia_intensidad = secciones[6].toFloat();
+  granizo = secciones[7].toFloat();//granizo acumulado e intensidad de granizo
+  granizo_intensidad = secciones[8].toFloat();
 
 
   msg.direccion = convertoFloatToUInt16(direccion, 360);
   msg.velocidad = convertoFloatToUInt16(velocidad, 50);
-  msg.temperatura = convertoFloatToUInt16(temperatura, 60, -60);
-  msg.humedad = convertoFloatToUInt16(humedad, 0, 100);
+  msg.temperatura = convertoFloatToUInt16(temperatura, 60, -20);
+  msg.humedad = convertoFloatToUInt16(humedad, 0, 110);
   msg.presion = convertoFloatToUInt16(presion, 1500, 500);
   msg_2.lluvia = convertoFloatToUInt16(lluvia, 1000);
   msg_2.intensidad_lluvia = convertoFloatToUInt16(lluvia_intensidad, 100);
   msg_2.granizo = convertoFloatToUInt16(granizo, 1000);
   msg_2.intensidad_granizo = convertoFloatToUInt16(granizo_intensidad, 100);
-  Serial.println(msg.temperatura);
-  Serial.println(msg_2.granizo);
+
   /********************************************************************************************************************
-  msg.status = 1;
-  msg_2.status = 255;
+    msg.status = 1;
+    msg_2.status = 255;
     Serial.println("iniciando el envío");
      //Start the module
       SigFox.begin();
@@ -155,9 +137,9 @@ while (Serial1.available()) {
       SigFox.write((uint8_t*)&msg, 12);
       msg.lastMessageStatus = SigFox.endPacket(true);
       SigFox.end();
-  
+
       Serial.println("primer envio realizado, segundo envio");
-  
+
       if (lluvia || granizo) {
         Serial.println("hay lluvia o granizo, enviando datos");
         SigFox.begin();
@@ -171,29 +153,27 @@ while (Serial1.available()) {
         SigFox.write((uint8_t*)&msg_2, 12);
         msg_2.lastMessageStatus = SigFox.endPacket(true);
         SigFox.end();
-        
-
-}
-
-/*******************************************************************************************************************
-if (debugger) {
-  muestratodo();
-}
-*/
-LowPower.sleep(freq_msg * 60 * 1000);
-//delay(5000);
 
 
+    }
+
+    /*******************************************************************************************************************
+    if (debugger) {
+    muestratodo();
+    }
+  */
+//  LowPower.sleep(freq_msg * 60 * 1000);
+  //delay(5000);
 }
 
 void muestratodo() {
   Serial.println("----------------------------------------------------------------------------------------------------");
   Serial.print("estoy recibiendo: " + respuesta);
-  //  for (int i = 0; i <= 9; i++) {
-  //    Serial.println(secciones[i]);
-  //  }
+    for (int i = 0; i <= 8; i++) {
+      Serial.println(secciones[i]);
+    }
   Serial.print("posicion de las comas:");
-  for (int i = 0; i <=20; i++) {
+  for (int i = 0; i <= 11; i++) {
     Serial.print(String(comas[i]) + ", ");
   }
   Serial.println();
@@ -202,9 +182,9 @@ void muestratodo() {
   Serial.println("direccion del viento: " + String(direccion));
   Serial.println("velocidad del viento: " + String(velocidad));
   Serial.println("Temperatura ambiente: " + String(temperatura));
-  Serial.println("humedad relativa: " + String(humedad));  
+  Serial.println("humedad relativa: " + String(humedad));
   Serial.println("Presión atmosférica: " + String(presion));
-    Serial.println("lluvia: " + String(lluvia));
+  Serial.println("lluvia: " + String(lluvia));
   Serial.println("intensidad de lluvia: " + String(lluvia_intensidad));
   Serial.println("granizo: " + String(granizo));
   Serial.println("intensidad de granizo: " + String(granizo_intensidad));
